6fd01cd076be0339c407e18ead8387742a391f28
rafa <cadavidocampo.rco@gmail.com>
2023-09-30 22:12:31 -0500

    lesson04

---
+++ lesson04/data_types.py
@@ -0,0 +1,111 @@
+# string data type
+
+#literal assigment
+first = 'rafa'
+last = 'cadavid'
+
+
+print(type(first))
+print(type(first) ==str)
+print(isinstance(first, str))
+
+#constructor 
+pizza = str("Pepperoni")
+print(type(pizza))
+print(type(pizza) ==str)
+print(isinstance(pizza, str))
+
+#concatenation
+fullname = first + " " + last
+print (fullname)
+
+fullname += "!"
+print (fullname)
+
+#casting
+decade = str(1980)
+print (type(decade))
+print (decade)
+
+statement = "i like rock music from the " + decade + "s."
+print (statement)
+
+
+#multiple line
+
+multiline = '''
+hey how are you?              
+
+i was just checking in
+
+                               all good?
+
+
+'''
+print (multiline)
+
+#Escapen expecial caracters
+
+sentence = 'I\'m back at work! \t hey! \n wheres\'s this at\\located?'
+print (sentence)
+
+
+#string methods
+
+print (first)
+print (first.lower())
+print (first.upper())
+print (first)
+
+print (multiline.title())
+print (multiline.replace("good","ok"))
+
+print(len(multiline))
+
+multiline += "                                 "
+multiline = "                      " + multiline
+print(len(multiline))
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+#build a manu
+
+title = "menu".upper()
+print (title.center(20,"="))
+print ("Coffe".ljust(16, ".") + "$1".rjust(4))
+print ("Muffin".ljust(16, ".") + "$2".rjust(4))
+print ("Chesecake".ljust(16, ".") + "$4".rjust(4))
+
+#string index values
+
+print(first[0])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+
+#Boolean data metodhs
+
+print(first.startswith("r"))
+print(first.endswith("a"))
+
+myvalue= True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+#numeric data type
+#integrater
+
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+
+#float type
+
+
+
