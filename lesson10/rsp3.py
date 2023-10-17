3c5f569afce6c5ee01cb21eb319ae87025ba6a55
rafa <cadavidocampo.rco@gmail.com>
2023-10-16 20:07:32 -0500

    up to lesson10

---
+++ lesson10/rsp3.py
@@ -0,0 +1,67 @@
+#rock paper scissors
+
+import sys
+import random
+
+from enum import Enum
+
+
+def play_rps():
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
+
+
+    print("")
+    playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+
+
+    if playerchoice not in ["1","2","3"]:
+        print("You must enter 1, 2 or 3.")
+        return play_rps()
+
+
+    player = int (playerchoice)
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+
+    print ("")
+    print ("Your choice "+ str(RPS(player)).replace('RPS.','') + ".")
+    print ("Python choice "+ str (RPS(computer)).replace('RPS.','') + ".")
+    print ("")
+
+    if player == 1 and computer == 3:
+        print("🥳You win!")
+    elif player == 2 and computer == 1:
+        print("🥳You win!")
+    elif player == 3 and computer == 2:
+        print("🥳You win!")
+    elif player ==  computer:
+        print("🫡 Tie game!")
+    else:
+        print("🐍 Python wins!")    
+
+    print("\n play again? ")
+
+    while True:
+        
+        playagain = input("\nY for yes or \nQ to quit\n") 
+
+        if playagain.lower() not in ["y","q"]:
+            continue
+        else: 
+            break
+
+    if playagain.lower() == "y":
+        return play_rps()
+    else:
+        print ("🥳🥳🥳")
+        print ("Thank you")
+        sys.exit("Byeee!")
+
+
+
+
+
+play_rps()
