5bbe1a648f8740a541cc946421ff40781928a592
RafaelCadaO <cadavidocampo.rco@gmail.com>
2024-03-11 21:38:24 -0500

    lesson116 finsh

--- lesson16/arcade.py
+++ lesson16/arcade.py
@@ -1,3 +1,5 @@
+#Arcade
+
 import sys
 import rps
 import guessnumber
@@ -32,3 +34,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Provides a personalized game experience")
     parser.add_argument('-n','--name',metavar='name',required=True,help='The name of the person playing the game.')
+
+    args = parser.parse_args()
+
+    gameplayer = play_game(args.name)
+    gameplayer()
